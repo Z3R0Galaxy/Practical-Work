3126d353bd02410056e5079cd1d0b22af4df0d18
Z3R0Galaxy <orlandogien@icloud.com>
2025-03-28 09:55:38 +1100

    Added Labs 120.3A-C

--- Lab 120.3A - Pseudocode to Python/lab_1203A.py
+++ Lab 120.3A - Pseudocode to Python/lab_1203A.py
@@ -1,1 +1,21 @@
-# Lab 120.3A - Pseudocode to Python
+count = 0
+num = 0
+
+def process_number(x):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+
+while num >= 0:
+    num = int(input("number: "))
+    if num >= 0:
+        process_number(num)
+        count += 1
+    else:
+        print("Negative number entered. Ending input.")
+print(f"Numbers processed:  {count}")
---
+++ Lab 120.3B - Flowcharts to Python/lab_1203B.py
@@ -0,0 +1,22 @@
+alpha_part = ["blue", "brown", "green", "orange", "pink", "purple", "red", "white", "yellow"]
+item = 'purple'
+
+lower_bound = 0
+upper_bound = len(alpha_part) - 1
+midpoint = 0
+
+found = False
+
+while not found and lower_bound <= upper_bound:
+    midpoint = (lower_bound + upper_bound) // 2
+    if alpha_part[midpoint] == item:
+        found = True
+    elif alpha_part[midpoint] < item:
+        lower_bound = midpoint + 1
+    else:
+        upper_bound = midpoint - 1
+
+if found:
+    print(f"Item found at index {midpoint}")
+else:
+    print("Item not found")
